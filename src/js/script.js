625e6c8bbae077c5ba1de9ab8b2a1bae9e4094c6
Marek Jarczewski <marek.jarczewski81@gmail.com>
2024-03-06 19:04:57 +0100

    ToDO: renderInMenu method added

--- src/js/script.js
+++ src/js/script.js
@@ -58,8 +58,22 @@ const select = {
 
       thisProduct.id = id;
       thisProduct.data = data;
-      
+
+      thisProduct.renderInMenu();
+
       console.log('new Product:', thisProduct);
+    }
+
+    renderInMenu(){
+      const thisProduct = this;
+
+      // generate HTML based on template
+
+      // create element using utils.createElementFromHTML
+
+      // find menu container
+
+      // add element to menu
     }
   }
 
