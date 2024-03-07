71bc44cba34af8b37d8c3790a21143b84685baf9
Marek Jarczewski <marek.jarczewski81@gmail.com>
2024-03-07 02:36:23 +0100

    Finished initAccordion() method

--- src/js/script.js
+++ src/js/script.js
@@ -84,6 +84,25 @@ const select = {
 
     initAccordion(){
       const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+      /* prevent default action for event */
+      event.preventDefault();
+      /* find active product (product that has active class) */
+      const activeProduct = document.querySelector(
+        select.all.menuProductsActive
+      );
+      /* if there is active product and it's not thisProduct.element, remove class active from it */
+      if (activeProduct && activeProduct !== thisProduct.element) {
+        activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+      }
+      /* toggle active class on thisProduct.element */
+      thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+      });
     }
   }
 
