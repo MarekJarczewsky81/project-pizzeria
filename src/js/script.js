6c15a2d3c92676cef1a07d5db7f8a38ae76ea4c0
Marek Jarczewski <marek.jarczewski81@gmail.com>
2024-07-04 12:21:11 +0200

    Fixed Accordion_method:script.js

--- src/js/script.js
+++ src/js/script.js
@@ -88,12 +88,12 @@ const select = {
       const thisProduct = this;
 
       /* find the clickable trigger (the element that should react to clicking) */
-      // const clickableTrigger = thisProduct.element.querySelector(
-      //   select.menuProduct.clickable
-      // );
+      const clickableTrigger = thisProduct.element.querySelector(
+        select.menuProduct.clickable
+      );
 
       /* START: add event listener to clickable trigger on event click */
-      thisProduct.accordionTrigger.addEventListener("click", function (event) {
+      clickableTrigger.addEventListener("click", function (event) {
         /* prevent default action for event */
         event.preventDefault();
         /* find active product (product that has active class) */
@@ -113,12 +113,52 @@ const select = {
     
     initOrderForm() {
       const thisProduct = this;
+
+      thisProduct.form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for (let input of thisProduct.formInputs) {
+        input.addEventListener("change", function () {
+          thisProduct.processOrder();
+        });
+      }
+
+      thisProduct.cartButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
       console.log('initOrderForm:', thisProduct);
     }
 
     processOrder()  {
       const thisProduct = this;
-      console.log("processOrder:", thisProduct);
+
+      // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log("formData", formData);
+
+      // set price to default price
+      let price = thisProduct.data.price;
+
+      // for every category (param)...
+      for (let paramId in thisProduct.data.params) {
+        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+        console.log(paramId, param);
+
+        // for every option in this category
+        for (let optionId in param.options) {
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+          console.log(optionId, option);
+        }
+      }
+
+      // update calculated price in the HTML
+      thisProduct.priceElem.innerHTML = price;
     }
 
     getElements() {
